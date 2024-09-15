01e4bbb2c3bb85ecbb6c4b1c2738253068cfc517
Rohail Nawaz <117741414+rohailk6@users.noreply.github.com>
2024-09-16 02:54:32 +0500

    Time: 3 ms (70.79%) | Memory: 12.1 MB (97.44%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        
+        int count = 0;
+
+        for(int i = 0; i<nums.size(); i++){
+
+            if(nums[i] == target){
+
+                return i;
+            }
+            else if(nums[i] > target)
+            {
+                return i;
+            }
+        }
+
+        return nums.size();
+    }
+};
